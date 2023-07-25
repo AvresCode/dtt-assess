ed4eabae815d194c96ffc30db605d7eebbd52063
AvresCode <servas230@gmail.com>
2023-07-25 17:28:43 +0200

    style navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <ul>
-      <li><router-link to="/">Houses</router-link></li>
-      <li><router-link to="/about">About</router-link></li>
+    <ul class="navbar">
+      <li><router-link to="/" exact class="nav-links">Houses</router-link></li>
+      <li><router-link to="/about" class="nav-links">About</router-link></li>
     </ul>
   </div>
 </template>
@@ -12,3 +12,25 @@ export default {
   name: 'Navbar',
 };
 </script>
+<style scoped>
+.navbar {
+  display: flex;
+  list-style-type: none;
+  padding: 1rem 5rem;
+  border-bottom: 0.2rem solid rgb(244, 242, 242);
+}
+.nav-links {
+  text-decoration: none;
+  margin-left: 3rem;
+  color: black;
+  padding: 0.8rem;
+  border-radius: 1rem;
+}
+.nav-links:hover {
+  background-color: rgb(249, 241, 239);
+}
+
+.router-link-active {
+  font-weight: bolder;
+}
+</style>
