897372b29cfea66af83f3fd38e09b74ea3b4b351
AvresCode <servas230@gmail.com>
2023-07-25 18:33:13 +0200

    fetch all houses from API

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,46 @@
 <template>
-  <h1>Homepage</h1>
+  <div>
+    <h1>Homepage</h1>
+    <div v-if="allHouses.length === 0">Loading...</div>
+    <div v-else>
+      <ul>
+        <li v-for="house in allHouses" :key="house.id">
+          <div>
+            <img :src="house.image" alt="House Image" />
+            <div>
+              <p>{{ house.address }}</p>
+              <p>{{ house.postal_code }} {{ house.city }}</p>
+            </div>
+            <div>
+              <p>Size: {{ house.size }}</p>
+              <p>Bedrooms: {{ house.bedrooms }}</p>
+              <p>Bathrooms: {{ house.bathrooms }}</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
   name: 'HomePage',
+  computed: {
+    ...mapState(['allHouses']),
+  },
+  methods: {
+    ...mapActions(['fetchAllHouses']),
+  },
+  created() {
+    this.fetchAllHouses();
+  },
+  //   async mounted() {
+  //     if (this.allHouses.length === 0) {
+  //       await this.fetchAllHouses();
+  //     }
+  //   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,30 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 const store = createStore({
   state: {
-    allHouses: null,
+    allHouses: [],
   },
   mutations: {
     setAllHouses: (state, housesData) => {
       state.allHouses = housesData;
+    },
+  },
+
+  actions: {
+    async fetchAllHouses({ commit }) {
+      const URL = 'https://api.intern.d-tt.nl/api/houses';
+
+      try {
+        const response = await axios.get(URL, {
+          headers: { 'X-Api-Key': 'zL6vg_sRSaZfwACpB3MGOUeclmF1kiXr' },
+        });
+        const housesData = response.data;
+        console.log('Fetched houses', housesData);
+        commit('setAllHouses', housesData);
+      } catch (e) {
+        console.log('Houses fetch error:', e);
+      }
     },
   },
 });
